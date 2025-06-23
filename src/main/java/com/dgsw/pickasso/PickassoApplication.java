7389b89065f69fe44e4bc22a0f644a945858b032
jiyun0220 <145185712+jiyun0220@users.noreply.github.com>
2025-06-23 15:04:23 +0900

    create project

---
+++ src/main/java/com/dgsw/pickasso/PickassoApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.pickasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickassoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickassoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/pickasso/PickassoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.pickasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickassoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
